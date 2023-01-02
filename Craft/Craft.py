8988d7a92ba591a761de792948b6216f25e5c87c
Miny2021 <mingyu1909@outlook.com>
2023-01-02 17:48:37 +0800

    现在可以通过修改在第67行的text来打印字符（只会打印第一个） 注意格式：text = {(坐标x,坐标y):"字符"}

--- Craft/Craft.py
+++ Craft/Craft.py
@@ -1,27 +1,48 @@
 import os
-
-def print_window(columns,lines,line3text,text):
+import time
+firsttext="Please Wait |"
+for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
+    firsttext += "-"
+    time.sleep(0.01)
+    print(f"{firsttext}\r",end="")
+def print_window(columns,lines,texts):
+    printed_space = True
     for line in range(lines-1):
         for column in range(columns-1):
+            for text in texts.items():
+                if line == text[0][0]:
+                    if column == text[0][1]:
+                        printed_space = False
+                        if printed_space == False:
+                            print(text[1][:1],end="")
             if line == 0 and column == 0:
+                printed_space = False
                 print("_",end="")
             if line == 0:
+                printed_space = False
                 print("_",end="")
             elif line == lines-2:
-                if column == 0:
+                printed_space = False
+                if column == 0 or column == columns - int(columns * 0.2):
                     print("|",end="")
                 elif column == columns-2:
                     print("|")
                 else:
                     print("_",end="")
-            elif column == 0:
+            elif column == 0 or column == columns - int(columns * 0.2):
+                printed_space = False
                 print("|",end="")
             elif column == columns-2:
+                printed_space = False
                 print("|")
-            
-            else:
-                print(" ",end="")
 
+            if printed_space:
+                print(" ",end="")
+            printed_space = True
+for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
+    firsttext += "-"
+    time.sleep(0.01)
+    print(f"{firsttext}\r",end="")
 
     
 def reset_size():
@@ -29,13 +50,34 @@ def reset_size():
     global win_line
     win_column = os.get_terminal_size().columns
     win_line = os.get_terminal_size().lines
-
+for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
+    firsttext += "-"
+    time.sleep(0.01)
+    print(f"{firsttext}\r",end="")
+def command_if(cmd):
+    None
+for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
+    firsttext += "-"
+    time.sleep(0.01)
+    print(f"{firsttext}\r",end="")
 def main():
     reset_size()
     global text
-    text = {(10,10):"a"}
-    
-    print_window(win_column, win_line, "Main Terminal Window", text)
-
+    global run
+    text = {}
+    run = True
+    while run:
+        os.system("cls")
+        print_window(win_column, win_line, text)
+        command = input("> ")
+        
+        if command == "exit":
+            run = False
+        command_if(command)
+for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5+26)):
+    firsttext += "-"
+    time.sleep(0.01)
+    print(f"{firsttext}\r",end="")
 if __name__ == '__main__':
+
     main()

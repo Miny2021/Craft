34831387def71cfb6187090f6bd1a8aed0801042
Miny2021 <mingyu1909@outlook.com>
2023-02-20 18:34:45 +0800

    /setblock (int : x) (int : y) (text [:1}) /del (int : x) (int : y)

--- Craft/Craft.py
+++ Craft/Craft.py
@@ -1,83 +1,70 @@
 import os
-import time
-firsttext="Please Wait |"
-for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
-    firsttext += "-"
-    time.sleep(0.01)
-    print(f"{firsttext}\r",end="")
-def print_window(columns,lines,texts):
-    printed_space = True
-    for line in range(lines-1):
-        for column in range(columns-1):
+def print_screen(texts,msg):
+    columns = os.get_terminal_size().columns+1
+    lines = os.get_terminal_size().lines
+    msg = msg[:columns-3]
+    msgforlong = 0
+    umsgforlong = 0
+    for line in range(1,lines):
+        for column in range(1,columns):
+            space=True
+            pntmsg = True
+            if (line == 1 or line == lines-1) and column != 1 and column !=columns-1:
+                print('=',end='')
+                space = False
+                pntmsg = False
+            if column == 1 or column == columns-1:
+                print('|',end='')
+                space = False
+                pntmsg = False
+            if line == lines-3 and column != 1 and column !=columns-1:
+                print('-',end='')
+                space = False
+                pntmsg = False
+            if msg != '' and column != 1 and column !=columns-1:
+                if line == lines-2:
+                    if column == 2:
+                        print(msg,end='')
+                        msglong=len(list(msg))
+                    if msgforlong < msglong:
+                        space = False
+                        pntmsg = False
+                        msgforlong +=1
             for text in texts.items():
-                if line == text[0][0]:
-                    if column == text[0][1]:
-                        printed_space = False
-                        if printed_space == False:
-                            print(text[1][:1],end="")
-            if line == 0 and column == 0:
-                printed_space = False
-                print("_",end="")
-            if line == 0:
-                printed_space = False
-                print("_",end="")
-            elif line == lines-2:
-                printed_space = False
-                if column == 0 or column == columns - int(columns * 0.2):
-                    print("|",end="")
-                elif column == columns-2:
-                    print("|")
-                else:
-                    print("_",end="")
-            elif column == 0 or column == columns - int(columns * 0.2):
-                printed_space = False
-                print("|",end="")
-            elif column == columns-2:
-                printed_space = False
-                print("|")
-
-            if printed_space:
-                print(" ",end="")
-            printed_space = True
-for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
-    firsttext += "-"
-    time.sleep(0.01)
-    print(f"{firsttext}\r",end="")
-
-    
-def reset_size():
-    global win_column
-    global win_line
-    win_column = os.get_terminal_size().columns
-    win_line = os.get_terminal_size().lines
-for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
-    firsttext += "-"
-    time.sleep(0.01)
-    print(f"{firsttext}\r",end="")
-def command_if(cmd):
-    None
-for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5)):
-    firsttext += "-"
-    time.sleep(0.01)
-    print(f"{firsttext}\r",end="")
+                if column == text[0][0] and pntmsg:
+                    if line == text[0][1]:
+                        print(text[1][:1],end='')
+                        space = False
+            if space:
+                print(' ',end='')
+                space=True                                   
 def main():
-    reset_size()
-    global text
-    global run
-    text = {}
-    run = True
-    while run:
-        os.system("cls")
-        print_window(win_column, win_line, text)
-        command = input("> ")
-        
-        if command == "exit":
-            run = False
-        command_if(command)
-for i in range(int((os.get_terminal_size().columns-len(list(firsttext)))/5+26)):
-    firsttext += "-"
-    time.sleep(0.01)
-    print(f"{firsttext}\r",end="")
-if __name__ == '__main__':
-
-    main()
+    msg = 'Welcome for used!'
+    texts = {}
+    while True:
+        if os.name == 'posix':
+            os.system('clear')
+        if os.name == 'nt':
+            os.system('cls')
+        print_screen(texts,msg)
+        command = input('>')
+        msg = ''
+        if command[:9] == '/setblock':
+            try:
+                cmd = command.split()
+                texts[int(cmd[1]),int(cmd[2])]=cmd[3]
+                msg = 'setblock'
+            except:
+                msg = 'The command of you input has got a error.'
+        if command[:4] == '/del':
+            try:
+                cmd = command.split()
+                if (int(cmd[1]),int(cmd[2])) in texts.keys():
+                    del texts[(int(cmd[1]),int(cmd[2]))]
+            except:
+                msg = 'The command of you input has got a error.'
+        if command[:5] == '/list':
+            msg = str(texts)
+        else:
+            msg = command
+main()
